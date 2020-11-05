bd4b3c48b102794e29a4ae8fcceaf6a67fbcb98b
bellaliu3001 <lhsblue@hotmail.com>
2020-11-05 23:02:03 +1100

    added global style to reset

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './static/globalstyle.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle/>
   </React.StrictMode>,
   document.getElementById('root')
 );
