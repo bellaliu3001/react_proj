304ad01c8eb5e5008c5f6eee8087d5975d58bb05
bellaliu3001 <lhsblue@hotmail.com>
2020-11-10 23:03:20 +1100

    better code practice

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actionTypes';
+
 const defaultState = {
     focused: false,
 };
@@ -5,11 +7,11 @@ const defaultState = {
 export default (state = defaultState, action) => {
     const newState = JSON.parse(JSON.stringify(state));
     switch(action.type){
-        case 'search_focus':
+        case actionTypes.SEARCH_FOCUS:
             return {
                 focused: true
             }
-        case 'search_blur':
+        case actionTypes.SEARCH_BLUR:
             return {
                 focused: false
             }
