31fa311a6b20f63fb05763943e6cea4b09b68479
bellaliu3001 <lhsblue@hotmail.com>
2020-11-11 23:19:27 +1100

    optimised code

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -21,8 +21,8 @@ import {
 class Header extends Component{
 
     getListArea = () => {
-        if(this.props.focused){
-            // console.log(this.props.list);
+        const { focused, list } = this.props;
+        if(focused){
             return (
             <SearchInfo>
                 <SearchInfoTitle>
@@ -31,7 +31,7 @@ class Header extends Component{
                 </SearchInfoTitle>
                 <SearchInfoList>
                     {
-                        this.props.list.map((item, i) => {
+                        list.map((item, i) => {
                             return <SearchInfoItem key={i}>{item}</SearchInfoItem>
                         })
                     }
@@ -45,6 +45,7 @@ class Header extends Component{
     }
 
     render(){
+        const { focused, handleInputFocus, handleInputBlur } = this.props;
         return (
             <HeaderWrapper>
                 <Logo />
@@ -57,17 +58,17 @@ class Header extends Component{
                     </NavItem>
                     <Searchwrapper>
                         <CSSTransition
-                            in={this.props.focused}
+                            in={focused}
                             timeout={300}
                             classNames="slide"
                         >
                             <NavSearch
-                                className={this.props.focused ? 'focused' : ''}
-                                onFocus={this.props.handleInputFocus}
-                                onBlur={this.props.handleInputBlur}
+                                className={focused ? 'focused' : ''}
+                                onFocus={ handleInputFocus }
+                                onBlur={ handleInputBlur }
                             ></NavSearch>
                         </CSSTransition>
-                        <i className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
+                        <i className={focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
                         { this.getListArea() }
                     </Searchwrapper>
                     <Addition>
