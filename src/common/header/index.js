45dedd7bcbec15c88c5188beaba171305a0ab5ed
bellaliu3001 <lhsblue@hotmail.com>
2020-11-12 23:24:53 +1100

    avoided unnecessary ajax call

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -65,7 +65,7 @@ class Header extends Component{
     }
 
     render(){
-        const { focused, handleInputFocus, handleInputBlur } = this.props;
+        const { focused, handleInputFocus, handleInputBlur, list } = this.props;
         return (
             <HeaderWrapper>
                 <Logo />
@@ -84,7 +84,7 @@ class Header extends Component{
                         >
                             <NavSearch
                                 className={focused ? 'focused' : ''}
-                                onFocus={ handleInputFocus }
+                                onFocus={ () => { handleInputFocus(list) } }
                                 onBlur={ handleInputBlur }
                             ></NavSearch>
                         </CSSTransition>
@@ -115,8 +115,10 @@ const mapStateToProps = (state) => {
 }
 const mapDispatchToProps = (dispatch) => {
     return {
-        handleInputFocus: () => {
-            dispatch(actionCreators.getList());
+        handleInputFocus: (list) => {
+            if(list.size === 0){
+                dispatch(actionCreators.getList());
+            }
             dispatch(actionCreators.searchFocus());
         },
         handleInputBlur: () => {
