b1fe8a3d024655868f6622df396fc4102b7267ce
bellaliu3001 <lhsblue@hotmail.com>
2020-11-12 23:40:07 +1100

    intro router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { HashRouter, Route, BrowserRouter } from 'react-router-dom';
 import Header from './common/header';
 import store from './store'
 class App extends Component {
@@ -7,6 +8,14 @@ class App extends Component {
     return (
       <Provider store={store}>
         <Header/>
+        {/* <HashRouter>
+          <Route exact path="/" render={() => (<div>Home</div>)}></Route>
+          <Route exact path="/detail" render={() => (<div>Detail</div>)}></Route>
+        </HashRouter> */}
+        <BrowserRouter>
+          <Route exact path="/" render={() => (<div>Home</div>)}></Route>
+          <Route exact path="/detail" render={() => (<div>Detail</div>)}></Route>
+        </BrowserRouter>
       </Provider>
     );
   }
